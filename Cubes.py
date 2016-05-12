5275f1f0ae481170eacffc9906ea49d0508ac90f
Joseph Nofech <jnofech@ualberta.ca>
2016-05-12 10:56:28 -0600

    Updated: can now select step size for calculating S_2.

--- Cubes.py
+++ Cubes.py
@@ -11,12 +11,13 @@ import math
 
 print('Welcome to Cubes! \n \nAvailable functions: \n  cubegen - generates a subcube. \n  structgen - generates a structure function map from a given subcube. \n  mapgen - generates some 2D maps of the structure function, for each "dv". \n  plotgen - generates a 1D plot of averaged structure function versus radius.')
 
-def cubegen(vmin,vmax,ymin,ymax,xmin,xmax, deltaX=30, deltaV=3):
+def cubegen(vmin,vmax,ymin,ymax,xmin,xmax):
 	"""Generates a subcube of the specified dimensions from the .fits file.
 
 	   Argument format: "(vmin,vmax, ymin,ymax, xmin,xmax.)"
 	   ^ These are the parameters of the desired subcube.
-	   WARNING: Selecting too large of a subcube will hugely increase processing time."""
+	   WARNING: Selecting too large of a subcube will hugely increase processing time.
+	   If you use a large cube, be sure to set deltadeltaX to be larger in structgen."""
 
 	cube = SpectralCube.read('paws_norot.fits')
 	data = cube.filled_data[:]   # Pulls "cube"'s information (position, spectral info (?)) into a 3D Numpy array.
@@ -24,31 +25,43 @@ def cubegen(vmin,vmax,ymin,ymax,xmin,xmax, deltaX=30, deltaV=3):
 	subcube = cube[vmin:vmax,ymin:ymax,xmin:xmax]
 	return subcube
 
-def structgen(subcube0, deltaX=30, deltaV=3):
+def structgen(subcube0, deltaX=30, deltaV=3, deltadeltaX=1, normalization=True):
 
 	"""Generates a 3D structure function map from a given subcube.
 	
-	   Argument format: "(<subcube>, deltaX (default=30), deltaV (default=3))".
-	   "deltaX" (default: 30) is the maximum value of dX and dY. "deltaV" 
-	   (default: 3) is the maximum value of dV.
-	
+	   Argument format: "(<subcube>, deltaX (default=30), deltaV (default=3)),
+	   deltadeltaX (default=1), normalization (default=False)".
+	   "deltaX" is the maximum value of dX and dY. "deltaV" is the maximum 
+	   value of dV. "deltadeltaX" is the step size along both dx and dy for the
+	   structure function calculation, and it should be a factor of dX.
+
+	   Enabling normalization will normalize the structure function within the [0,1]
+	   interval. Disabling normalization will prevent this.
+
 	   Be aware that processing time will increase with large deltaX and deltaV 
-	   values."""
+	   values, but can decrease with larger deltadeltaX at the cost of plot
+	   resolution."""
 
 	data0 = subcube0.filled_data[:]
 
 	dX = deltaX                    	# This is simply the maximum absolute value of "dx". So if dX = 1, then dx = {-1,0,1}.
 	dY = np.copy(dX)                # Same as above, but for "dy". For simplicity, let it be the same as dX.
 	dZ = deltaV		     	# Same as above, but for "dv". We call it "dZ" here since that may make it easier to visualize.
-	nmax = abs(2*dX)+1
-	S_2 = np.zeros([abs(2*dZ)+1,nmax,nmax])
+	ddX = deltadeltaX		# "Step size", for the S_2 calculation. Make sure it's a factor of dX. Leave at 1 for full calculation.
+	ddY = np.copy(ddX)            	# Same as above, but for dY.
+	nmax = abs(2*dX/ddX)+1
+	S_2 = np.zeros([dZ+1,nmax,nmax])
 
 	p,n,m = data0.shape     # The subcube has n rows, m columns, and p "height units".
 
 
-	for dz in range (0,dZ+1):
-	    for dx in range (-dX,dX+1):
-		for dy in range (-dY,dY+1):
+	for delz in range (0,dZ+1):                     # "delz" defined as "dz / ddZ"
+	    for delx in range (-dX/ddX,dX/ddX+1):       # "delx" defined as "dx / ddX"
+		for dely in range (-dY/ddY,dY/ddY+1):   # "dely" defined as "dy / ddY"
+
+		    dx = delx*ddX
+		    dy = dely*ddY
+		    dz = delz				# ddZ = 1. Cannot be changed in this program.
 		
 		    M = data0.value         		# This is the subcube information (unitless).
 		    P = np.arange(p*n*m).reshape(p,n,m) # This will be used to track the shifting "pixels" of M(r) and M(r+dr).
@@ -64,11 +77,15 @@ def structgen(subcube0, deltaX=30, deltaV=3):
 			    #	    Similar case for a pixel "dz" layers below.
 			    #
 		            # In "goodpix", pixels that have wrapped around are treated as "False".
+		    if normalization==True:
+			    OrigMapPower = np.nanmean(M[goodpix]**2)
+			    RollMapPower = np.nanmean(RollMap[goodpix]**2)
 
-		    OrigMapPower = np.nanmean(M[goodpix]**2)
-		    RollMapPower = np.nanmean(RollMap[goodpix]**2)
+			    S_2[(dz,(dy+dY)/ddY,(dx+dX)/ddX)] = (np.nanmean(D[goodpix]**2)) / (OrigMapPower + RollMapPower)
+		    else:
+			    S_2[(dz,(dy+dY)/ddY,(dx+dX)/ddX)] = (np.nanmean(D[goodpix]**2))
 
-		    S_2[(dz,dy+dY,dx+dX)] = (np.nanmean(D[goodpix]**2)) / (OrigMapPower + RollMapPower)
+
 	return S_2
 
 
@@ -102,12 +119,13 @@ def mapgen(S_2, deltaX=30, deltaV=3):
 	plt.colorbar()
 	plt.savefig('image3DcubeV2.png')
 
-def plotgen(S_2, deltaX=30, deltaV=3):
+def plotgen(S_2, deltaX=30, deltaV=3, deltadeltaX=1):
 	"""Generates and saves a 1D plot of the azimuthally-averaged structure function versus 
 	   radius, for each value of "dv".
 
-	   Argument format: (S_2, deltaX). Plots are created using the resulting 3D
-	   matrix from structgen, and the same deltaX that was used in structgen."""
+	   Argument format: (S_2, deltaX, deltaV, deltadeltaX). Plots are created using the 
+	   resulting 3D matrix from structgen, and the same deltaX, deltaV, deltadeltaX
+	   that were used in structgen."""
 
 	# Goal: Create a 1D plot, for each value of "dv", of the average value of structure function (inside a thin ring
 	#       at radius r) versus radius. Each "dv" is a different sheet of dx,dy.
@@ -115,13 +133,15 @@ def plotgen(S_2, deltaX=30, deltaV=3):
 	dX = deltaX                    	# This is simply the maximum absolute value of "dx". So if dX = 1, then dx = {-1,0,1}.
 	dY = np.copy(dX)                # Same as above, but for "dy". For simplicity, let it be the same as dX.
 	dZ = deltaV		     	# Same as above, but for "dv". We call it "dZ" here since that may make it easier to visualize.
-	nmax = abs(2*dX)+1
+	ddX = deltadeltaX
+	ddY = np.copy(ddX)
+	nmax = abs(2*dX/ddX)+1
 
-	x = np.linspace(-dX,dX,nmax)
-	y = np.linspace(-dY,dY,nmax)
+	x = np.linspace(-dX/ddX,dX/ddX,nmax)
+	y = np.linspace(-dY/ddY,dY/ddY,nmax)
 	xx, yy = np.meshgrid(x,y)
 
-	maxradius = math.floor( (dX**2 + dY**2)**0.5 )
+	maxradius = ( (dX/ddX)**2 + (dY/ddY)**2 )**0.5
 	mult = 1                        # Increases or decreases the numbers of bins used. Most accurate results at mult=1.
 	reselements = math.floor(mult*maxradius)
 		                        # This is the number of "resolution elements" (i.e. the number of points
@@ -135,11 +155,12 @@ def plotgen(S_2, deltaX=30, deltaV=3):
 		radiusmap[radiusmap<maxradius], S_2[i][radiusmap<maxradius], statistic=np.nanmean, bins = reselements)
 
 	plt.figure(2)
+	X = (np.arange(reselements)/mult) / ((reselements-1)/mult) * (dX**2 + dY**2)**0.5
 	for i in range (0, dZ+1):
-	    plt.plot(np.arange(reselements)/mult,struct_funct[i],label='dv='+str(i))
+	    plt.plot(X, struct_funct[i],label='dv='+str(i))
 	plt.title('Avg. Struct. Funct. vs. Radial "Distance" from Center of S_2 Plots')
 	plt.xlabel(' "Radius" ')
 	plt.ylabel('Average S_2')
-	plt.ylim([0.9,1.1])
+#	plt.ylim([0.9,1.1])
 	plt.legend(loc='lower right')
 	plt.savefig('plot3DcubeV2.png')
